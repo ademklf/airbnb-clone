3ebbb1094dd26487228fac64df99899fc2bd4527
adem kalfalı <ademkalfa9@gmail.com>
2023-03-12 10:15:45 +0300

    Header.js add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build AIRBNB clone</h1>
-
+      <Header />
       <Home />
 
       {/*Home*/}
@@ -20,7 +20,7 @@ function App() {
       {/*Footer*/}
 
       {/*SearchPAge*/}
-
+      {/*Header*/}
       {/*...*/}
     </div>
   );
