25a09c393c9e4a7d18e924f53c4a844abff2b3b4
adem kalfalı <ademkalfa9@gmail.com>
2023-03-12 14:19:49 +0300

    Header.css finished and start banner

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
   return (
-    <div className="Header">
+    <div className="header">
       <img
         className="header__icon"
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
@@ -14,6 +17,13 @@ function Header() {
       <div className="header__center">
         <input type="text" />
         <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
       </div>
     </div>
   );
