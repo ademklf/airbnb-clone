5ea66fc9c38368ba854464fefb46666531537037
adem kalfalı <ademkalfa9@gmail.com>
2023-03-13 11:23:23 +0300

    BannerSearchButton Add

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,15 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./Banner.css";
 
 function Banner() {
   return (
     <div className="banner">
+      <div className="banner__search">
+        <Button className="banner__searchButton" variant="outlined">
+          Search Dates
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and strech your imagination</h1>
         <h5>
