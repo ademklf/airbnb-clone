70d86248361ccdfff32cb8d5a7a5c18f4cba1e85
adem kalfalı <ademkalfa9@gmail.com>
2023-03-16 12:43:31 +0300

    searchPage-add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
 
-      {/*Home*/}
+        <Routes>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
 
-      {/*Header*/}
-
-      {/*Banner*/}
-      {/*Search*/}
-
-      {/*Card*/}
-
-      {/*Footer*/}
-
-      {/*SearchPAge*/}
-      {/*Header*/}
-      {/*...*/}
+        <Footer />
+      </Router>
     </div>
   );
 }
