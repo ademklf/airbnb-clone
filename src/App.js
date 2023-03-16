5a0790a2634f983cd3ac86654e78ea35a2df5c3c
adem kalfalı <ademkalfa9@gmail.com>
2023-03-16 14:37:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-      <Router>
+      <Router basename="/airbnb-clone">
         <Header />
 
         <Routes>
-          <Route path="/airbnb-clone" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/search" element={<SearchPage />}></Route>
         </Routes>
 
